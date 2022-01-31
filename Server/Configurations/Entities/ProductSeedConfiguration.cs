ea9495c4e04d57319993c815a41ec547c8b0f147
NUR AISHUARYNIE A'QIDAH BINTE MOHAMMAD AMIRRUDIN 2005124I <2005124I@student.tp.edu.sg>
2022-01-31 16:46:02 +0800

    sad

---
+++ Server/Configurations/Entities/ProductSeedConfiguration.cs
@@ -0,0 +1,30 @@
+﻿//using System;
+//using Microsoft.EntityFrameworkCore;
+//using ProjectTech.Shared.Domain;
+
+//namespace ProjectTech.Server.Configurations.Entities
+//{
+//    public class ProductSeedConfiguration : IEntityTypeConfiguration<Product>
+//    {
+//        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Product> builder)
+//        {
+//            builder.HasData(
+//            new Product
+//            {
+//                ProductId = 1,
+//                CustomerId = 1,
+//                Price = "",
+//                Name = "Administrator",
+//                NormalizedName = "ADMINISTRATOR"
+//            },
+
+//             new Product
+//             {
+//                 ProductId = 2,
+//                 Name = "User",
+//                 NormalizedName = "USER"
+//             }
+// );
+//        }
+//    }
+//}
